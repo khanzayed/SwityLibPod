53e3a2240c906d04d73b58743aaaad18d496ad29
Faraz <faraz@orient.com.au>
2020-08-12 16:53:19 +0530

    first commit

---
+++ SwiftyLibExamples/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SwiftyLibExamples
+//
+//  Created by Faraz Habib on 12/8/20.
+//  Copyright © 2020 Faraz Habib. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
