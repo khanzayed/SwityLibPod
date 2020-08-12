97bcfbf5fd8e912ba76bb3b5db2bd1bf153f896a
Faraz <faraz@orient.com.au>
2020-08-12 17:06:08 +0530

    Added files

---
+++ SwiftyLib/AnalyticsHome.swift
@@ -0,0 +1,19 @@
+//
+//  AnalyticsHome.swift
+//  SwiftyLib
+//
+//  Created by Faraz Habib on 12/8/20.
+//  Copyright © 2020 Faraz Habib. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+
+public class AnalyticsHome {
+    
+    public func logEvents() {
+        print("Started logging events")
+    }
+    
+}
